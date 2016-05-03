50b34b68c70e3432c3619c3a1d8c3d50ecb0381e
Peter Souter <p.morsou@gmail.com>
2016-05-03 18:26:17 +0100

    Add Fedora support

---
+++ manifests/repo/fedora.pp
@@ -0,0 +1,31 @@
+class cockpit::repo::fedora {
+
+  if $::cockpit::yum_preview_repo {
+
+    yumrepo { 'group_cockpit-cockpit-preview':
+      ensure              => 'present',
+      baseurl             => 'https://copr-be.cloud.fedoraproject.org/results/@cockpit/cockpit-preview/fedora-$releasever-$basearch/',
+      descr               => 'Copr repo for cockpit-preview owned by msuchy',
+      enabled             => '1',
+      gpgcheck            => '1',
+      gpgkey              => 'https://copr-be.cloud.fedoraproject.org/results/@cockpit/cockpit-preview/pubkey.gpg',
+      skip_if_unavailable => 'True',
+    }
+
+  } else {
+
+    yumrepo { 'updates':
+      ensure              => 'present',
+      descr               => 'Fedora $releasever - $basearch - Updates',
+      enabled             => '1',
+      failovermethod      => 'priority',
+      gpgcheck            => '1',
+      gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-$releasever-$basearch',
+      metadata_expire     => '6h',
+      metalink            => 'https://mirrors.fedoraproject.org/metalink?repo=updates-released-f$releasever&arch=$basearch',
+      skip_if_unavailable => 'False',
+    }
+
+  }
+
+}
