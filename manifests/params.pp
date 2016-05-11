8b245736baa9f97970b40d5f4f4caabbff42aa74
Peter Souter <p.morsou@gmail.com>
2016-05-11 16:55:59 +0100

    Move non-RedHat specific parameters to global
    * So far the only thing that's RedHat specific is the Yum preview repos
    * Everything else is the same across various distros

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,21 +1,27 @@
 # cockpit::params - Default parameters
 class cockpit::params {
+
+  # OS Specific Defaults
   case $::osfamily {
     'RedHat': {
-      $manage_repo     = true
       $yum_preview_repo = false
-      $manage_package  = true
-      $package_name    = 'cockpit'
-      $package_version = 'installed'
-      $service_name    = 'cockpit'
-      $manage_service  = true
-      $service_ensure  = 'running'
-      $logintitle      = $::fqdn
-      $allowunencrypted = false
-      $maxstartups      = '10'
     }
     default: {
       fail("${::operatingsystem} not supported")
     }
   }
+
+  # Defaults for all Operating Systems
+  # (Cockpit has consistant naming accross OS's, hooray! :D)
+  $allowunencrypted = false
+  $logintitle       = $::fqdn
+  $manage_package   = true
+  $manage_repo      = true
+  $manage_service   = true
+  $maxstartups      = '10'
+  $package_name     = 'cockpit'
+  $package_version  = 'installed'
+  $service_ensure   = 'running'
+  $service_name     = 'cockpit'
+
 }
