267060e40b8c34f5ae3da2d7fbc347e7757de848
Peter Souter <peter.souter+GIT@puppet.com>
2017-03-07 10:30:01 +0000

    Changes Ubuntu repo to apt::source
    * Prefer over ppa's, which are execs
    * Easier to run specs on

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -2,8 +2,15 @@ class cockpit::repo::ubuntu {
 
   contain ::apt
 
-  ::apt::ppa { 'ppa:cockpit-project/cockpit':
-    before     => Class['apt::update'],
+  ::apt::source { 'cockpit':
+    location => 'http://ppa.launchpad.net/cockpit-project/cockpit/ubuntu',
+    release  => $::lsbdistcodename,
+    repos    => 'main',
+    key      => {
+      id     => '637A2C82EDB1EF02DA658EE1046452EBC99782CC',
+      server => 'keyserver.ubuntu.com',
+    },
+    before   => Class['apt::update'],
   }
 
 }
